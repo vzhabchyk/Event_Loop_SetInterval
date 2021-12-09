ea727a018f65993de7701d537c6559c6ece137ab
vzhabchyk <rvromanovna@gmail.com>
2021-12-09 12:49:13 +0200

    second commit

--- js/app.js
+++ js/app.js
@@ -4,7 +4,8 @@ function getDigits(number) {
   return [firstDigit, secondDigit];
 }
 
-function updateTime (type, digits) {
+function updateTime (type, number) {
+  const digits = getDigits (number);
   const [clockDigitOne, clockDigitTwo] = document.getElementsByClassName(type);
   clockDigitOne.src = `./images/img_${digits[0]}.png`;
   clockDigitTwo.src = `./images/img_${digits[1]}.png`;
@@ -12,23 +13,37 @@ function updateTime (type, digits) {
 
 function timer (hour, min, sec) {
   setInterval(() => {
-    
-  }
-  , 1000)
+    ++sec;
+    if (sec === 60) {
+      ++min;
+      sec = 0;
+    }
+
+    if (min === 60) {
+      ++hour;
+      min = 0;
+    }
+
+    if (hour === 24) {
+      hour = 0;
+    }
+
+    updateTime('hour', hour);
+    updateTime('min', min);
+    updateTime('sec', sec);
+  }, 1000);
 }
 
 function currentTime () {
-  let date = new Date ();
-  let hour = date.getHours();
-  hour = getDigits(hour);
-  let min = date.getMinutes();
-  min = getDigits(min);
-  let sec = date.getSeconds();
-  sec = getDigits(sec);
-
+  const date = new Date ();
+  const hour = date.getHours();
+  const min = date.getMinutes();
+  const sec = date.getSeconds();
+  
   updateTime('hour', hour);
   updateTime('min', min);
   updateTime('sec', sec);
+  timer(hour, min, sec);
 }
 currentTime();
 
