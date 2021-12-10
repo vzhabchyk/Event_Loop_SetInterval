703c12bee5489c2349c9c921df43d864d63ecd97
vzhabchyk <rvromanovna@gmail.com>
2021-12-10 17:50:51 +0200

    fifth commit

--- part_2/js/app.js
+++ part_2/js/app.js
@@ -7,29 +7,75 @@ const arrayImgs = [
 ];
 
 let currentSlide = 0;
-const slideElem = document.getElementById('slide');
-const btnPrev = document.getElementsByClassName('btn-prev')[0];
-const btnNext = document.getElementsByClassName('btn-next')[0];
+let intervalId;
 
-changeSlide();
-
-function prevSlide () {
-  currentSlide -= 1;
-  changeSlide();
-}
-function nextSlide () {
-  currentSlide += 1;
-  changeSlide();
-}
-
-function changeSlide () {
+function setSlideImages () {
+  const slideElem = document.getElementById('slide');
   slideElem.src = arrayImgs[currentSlide];
-  if (currentSlide === 0) {
-    btnPrev.classList.add('hidden');
-  } else if (currentSlide === arrayImgs.length - 1){
-    btnNext.classList.add('hidden');
-  } else {
-    btnPrev.classList.remove('hidden');
-    btnNext.classList.remove('hidden');
-  }
 }
+setSlideImages();
+
+function changeSlide(iteration = 1) {
+  currentSlide += iteration;
+  if (currentSlide === arrayImgs.length) {
+    currentSlide = 0;
+  }
+  if (currentSlide < 0) {
+    currentSlide = arrayImgs.length - 1;
+  }
+  console.log(currentSlide);
+  setSlideImages();
+}
+
+function sliderTimer () {
+  intervalId = setInterval (changeSlide, 3000);
+}
+sliderTimer();
+
+document.getElementsByClassName('btn-prev')[0].addEventListener('click', function() {
+  clearInterval(intervalId)
+  changeSlide(-1);
+  sliderTimer();
+});
+
+document.getElementsByClassName('btn-next')[0].addEventListener('click', function() {
+  clearInterval(intervalId)
+  changeSlide(1);
+  sliderTimer();
+});
+
+
+
+
+
+
+
+
+
+// let currentSlide = 0;
+// const slideElem = document.getElementById('slide');
+// const btnPrev = document.getElementsByClassName('btn-prev')[0];
+// const btnNext = document.getElementsByClassName('btn-next')[0];
+
+// changeSlide();
+
+// function prevSlide () {
+//   currentSlide -= 1;
+//   changeSlide();
+// }
+// function nextSlide () {
+//   currentSlide += 1;
+//   changeSlide();
+// }
+
+// function changeSlide () {
+//   slideElem.src = arrayImgs[currentSlide];
+//   if (currentSlide === 0) {
+//     btnPrev.classList.add('hidden');
+//   } else if (currentSlide === arrayImgs.length - 1){
+//     btnNext.classList.add('hidden');
+//   } else {
+//     btnPrev.classList.remove('hidden');
+//     btnNext.classList.remove('hidden');
+//   }
+// }
